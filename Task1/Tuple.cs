3166b72ab4a60c6fdf0044cdfe7e0dcf4efc4221
Yeghor Danyliuk <egordan74@gmail.com>
2025-11-02 18:36:33 +0200

    Add project files.

---
+++ Task1/Tuple.cs
@@ -0,0 +1,27 @@
+﻿namespace Task1
+{
+    public readonly record struct StudentEntry(string Surname, double Gpa, bool IsPaid, bool RetookExams);
+    public class Tuple
+    {
+        public static bool IsGonnaTakeScholarship(StudentEntry student)
+        {
+            if ((student.Gpa >= 4.5) && (!student.IsPaid) && (!student.RetookExams))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static int CalculateScholarships(List<StudentEntry> students)
+        {
+            int count = 0;
+            foreach (StudentEntry student in students)
+            {
+                if (IsGonnaTakeScholarship(student))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
